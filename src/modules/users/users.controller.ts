de02565d8d3d888e25e43dc5b0ceab2105a51510
Bogdan <bohdanmatv1@gmail.com>
2021-11-01 13:11:16 +0200

    basic user API

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -9,6 +9,21 @@ export class UsersController {
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+  
+  @Post('register')
+  create(@Req() req) {
+    return this.usersService.create(req);
+  }
+  
+  @Patch(':id')
+  update(@Req() req) {
+    return this.usersService.update(req)
+  }
+  
+  @Delete(':id')
+  delete(@Req() req) {
+    return this.usersService.delete(req);
   }
 
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,13 +1,48 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { User } from './user.entity';
-import { of } from 'rxjs';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class UsersService {
-  constructor( @Inject('USERS_REPOSITORY') private usersRepository: typeof User) {
+  constructor(@Inject('USERS_REPOSITORY') private usersRepository: typeof User) {
   }
   
-  async findAll(){
+  async findAll() {
     return this.usersRepository.findAll<User>();
+  }
+  
+  async findOne(id) {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+  
+  async create(req) {
+    return this.usersRepository.create({
+      id: uuid(),
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      phone: req.body.phone,
+      email: req.body.email
+    });
+  }
+  
+  async update(req) {
+    return this.usersRepository.update(
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone,
+        email: req.body.email
+      },
+      {
+        where: { id: req.params.id }
+      })
+      .then(() => this.findOne(req.params.id));
+  }
+  
+  async delete(req) {
+    return this.usersRepository.destroy(
+      { where: { id: req.params.id } }
+    )
+      .then(() => this.findAll());
   }
 }
