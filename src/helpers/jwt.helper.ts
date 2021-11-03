0a3aed5ddc6f91cda645f356bab96bbdadf271e5
Bogdan <bohdanmatv1@gmail.com>
2021-11-03 15:38:03 +0200

    minort refs

--- src/helpers/jwt.helper.ts
+++ src/helpers/jwt.helper.ts
@@ -2,8 +2,9 @@ const jwt = require('jsonwebtoken');
 
 export class JwtHelper {
   
-  async assignToken(user): Promise<string> {
-    return await jwt.sign({ data: user }, process.env.JWT_SECRET);
+  async assignToken(user): Promise<{ token: string }> {
+    const token = await jwt.sign({ data: user }, process.env.JWT_SECRET);
+    return { token };
   }
   
   async verifyToken(token) {
